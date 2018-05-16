3e6e5c4e681b30be9bd5192f502f565926738f39
Paul <takao@mailbox.org>
2018-05-16 15:53:17 +0200

    Fixed Projekt

---
+++ Source/CoopArena/Private/Interfaces/Interactable.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interactable.h"
+
+Interactable::Interactable()
+{
+}
+
+Interactable::~Interactable()
+{
+}
