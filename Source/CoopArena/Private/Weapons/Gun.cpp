11a28a2758e6681d1a580edb3a92e2394e114cae
Paul <takao@mailbox.org>
2019-01-05 12:33:25 +0100

    commit

--- Source/CoopArena/Private/Weapons/Gun.cpp
+++ Source/CoopArena/Private/Weapons/Gun.cpp
@@ -58,7 +58,7 @@ AGun::AGun()
 	_HorizontalSpreadToApply = 0.0f;
 	_GunStats.KickbackSpeed = 10.0f;
 
-	_DespawnTime = 60.0f;
+	_DespawnTime = 30.0f;
 
 	bReplicates = true;
 	bReplicateMovement = false;
@@ -377,9 +377,6 @@ void AGun::AttachMeshToPawn()
 
 			const FVector offsetLoc = -gripPointTransform.InverseTransformPosition(_EquipOffset->GetComponentLocation());
 			_Mesh->SetRelativeLocation(offsetLoc);
-
-			/*FString conType = HasAuthority() ? "Server" : "Client";
-			UE_LOG(LogTemp, Warning, TEXT("[%s] Rotation: %s. Offset: %s"), *conType, *offsetRot.ToString(), *offsetLoc.ToString());*/
 		}
 	}
 }
@@ -632,7 +629,15 @@ UCameraComponent* AGun::GetZoomCamera() const
 /////////////////////////////////////////////////////
 void AGun::StartRespawnTimer()
 {
-	GetWorld()->GetTimerManager().SetTimer(_DespawnTH, [&]() {Destroy(); }, _DespawnTime, false);
+	GetWorld()->GetTimerManager().SetTimer(_DespawnTH, [&]()
+	{
+		if (_LoadedMagazine)
+		{
+			_LoadedMagazine->Destroy();
+		}
+		Destroy();
+	}, 
+	_DespawnTime, false);
 }
 
 void AGun::StopRespawnTimer()
@@ -917,4 +922,3 @@ void AGun::HandleMuzzleFlash_Multicast_Implementation(bool bSpawnMuzzleFlash)
 {
 	HandleMuzzleFlash(bSpawnMuzzleFlash);
 }
-
